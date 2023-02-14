1751e37f6ea566ff7962cdb308689a22adfb5316
Simarjeet Singh <sjthecoder@gmail.com>
2023-02-15 02:13:13 +0530

    submit handler initial implementation

--- src/components/CS/StickerForm.tsx
+++ src/components/CS/StickerForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import FormWrapper from "../FormWrapper";
 
@@ -7,7 +7,21 @@ const checkBoxWrapper = "flex gap-2 items-center justify-center text-gray-200";
 const spanText = "font-semibold text-white";
 
 const StickerForm = (props: Props) => {
-	const submitHandler = () => {};
+	const stickerName = useRef<HTMLInputElement>(null);
+	const [isTS, setisTS] = useState(false);
+	const tName = useRef<HTMLInputElement>(null);
+	const [grade, setGrade] = useState({
+		Paper: false,
+		Foil: false,
+		Glitter: false,
+		Holo: false,
+		Gold: false,
+	});
+
+	const submitHandler = (e: FormEvent) => {
+		e.preventDefault();
+		console.log(stickerName.current?.value, isTS, tName.current?.value, grade);
+	};
 
 	return (
 		<motion.div
@@ -23,34 +37,69 @@ const StickerForm = (props: Props) => {
 			>
 				<div className="flex flex-col gap-4">
 					<span className={spanText}>Sticker Name</span>
-					<input type="text" className="justify-end" />
+					<input type="text" className="justify-end" ref={stickerName} />
 				</div>
 				<div className="flex items-center justify-center gap-4">
 					<span className={spanText}>Tournament Sticker</span>
-					<input type="checkbox" />
-					<input type="text" />
+					<input
+						type="checkbox"
+						onChange={(e: ChangeEvent<HTMLInputElement>) => {
+							setisTS(e.target.checked);
+						}}
+					/>
+					<input
+						type="text"
+						ref={tName}
+						disabled={!isTS}
+						className="disabled:cursor-not-allowed"
+					/>
 				</div>
 				<div className="flex flex-col gap-4">
 					<span className={spanText}>Grade</span>
 					<div className="ml-4 flex gap-4">
 						<div className={checkBoxWrapper}>
-							<input type="checkbox" />
+							<input
+								type="checkbox"
+								onChange={(e: ChangeEvent<HTMLInputElement>) => {
+									setGrade({ ...grade, Paper: e.target.checked });
+								}}
+							/>
 							<span>Paper</span>
 						</div>
 						<div className={checkBoxWrapper}>
-							<input type="checkbox" />
+							<input
+								type="checkbox"
+								onChange={(e: ChangeEvent<HTMLInputElement>) => {
+									setGrade({ ...grade, Foil: e.target.checked });
+								}}
+							/>
 							<span>Foil</span>
 						</div>
 						<div className={checkBoxWrapper}>
-							<input type="checkbox" />
+							<input
+								type="checkbox"
+								onChange={(e: ChangeEvent<HTMLInputElement>) => {
+									setGrade({ ...grade, Glitter: e.target.checked });
+								}}
+							/>
 							<span>Glitter</span>
 						</div>
 						<div className={checkBoxWrapper}>
-							<input type="checkbox" />
+							<input
+								type="checkbox"
+								onChange={(e: ChangeEvent<HTMLInputElement>) => {
+									setGrade({ ...grade, Holo: e.target.checked });
+								}}
+							/>
 							<span>Holo</span>
 						</div>
 						<div className={checkBoxWrapper}>
-							<input type="checkbox" />
+							<input
+								type="checkbox"
+								onChange={(e: ChangeEvent<HTMLInputElement>) => {
+									setGrade({ ...grade, Gold: e.target.checked });
+								}}
+							/>
 							<span>Gold</span>
 						</div>
 					</div>
