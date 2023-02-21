ca174ab9947668c440b14622315b15d2d81c3b26
Simarjeet Singh <sjthecoder@gmail.com>
2023-02-21 13:27:18 +0530

    animate presenence row

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
 import { gun, sticker } from "../typeModels/models";
+import { AnimatePresence, motion } from "framer-motion";
 
 <tr>
 	<td className="p-4 pl-4 text-left">Yo</td>
@@ -13,7 +14,7 @@ export default function SummaryTable() {
 	const summaryData = useSelector((state: RootState) => state.csSummary);
 	return (
 		<div className="flex h-full w-full flex-col overflow-hidden rounded bg-neutral-700 text-white shadow-md shadow-cyan-800">
-			<table className="table-auto pl-4">
+			<table className="table-fixed pl-4">
 				<thead className="sticky border-b-2 border-gray-300">
 					<tr>
 						<th className="p-4 text-left text-lg">Name</th>
@@ -22,25 +23,39 @@ export default function SummaryTable() {
 					</tr>
 				</thead>
 				<tbody className="divide-y-2 divide-gray-600">
-					{Object.values(summaryData).map((x: gun | sticker) => {
-						if ("isStatTrak" in x) {
-							return (
-								// Gun
-								<tr key={x.name}>
-									<td className="p-4 pl-4 text-left">{x.name}</td>
-									<td className="p-4 pl-4 text-left">{x.type}</td>
-								</tr>
-							);
-						} else {
-							//  Sticker
-							return (
-								<tr key={x.name}>
-									<td className="p-4 pl-4 text-left">{x.name}</td>
-									<td className="p-4 pl-4 text-left">{x.type}</td>
-								</tr>
-							);
-						}
-					})}
+					<AnimatePresence>
+						{Object.values(summaryData).map((x: gun | sticker) => {
+							if ("isStatTrak" in x) {
+								return (
+									// Gun
+									<motion.tr
+										key={x.name}
+										initial={{ y: 10, opacity: 0 }}
+										animate={{ y: 0, opacity: 1 }}
+										exit={{ y: -10, opacity: 0 }}
+										transition={{ duration: 0.2 }}
+									>
+										<td className="p-4 pl-4 text-left">{x.name}</td>
+										<td className="p-4 pl-4 text-left">{x.type}</td>
+									</motion.tr>
+								);
+							} else {
+								//  Sticker
+								return (
+									<motion.tr
+										key={x.name}
+										initial={{ y: 10, opacity: 0 }}
+										animate={{ y: 0, opacity: 1 }}
+										exit={{ y: -10, opacity: 0 }}
+										transition={{ duration: 0.2 }}
+									>
+										<td className="p-4 pl-4 text-left">{x.name}</td>
+										<td className="p-4 pl-4 text-left">{x.type}</td>
+									</motion.tr>
+								);
+							}
+						})}{" "}
+					</AnimatePresence>
 				</tbody>
 			</table>
 		</div>
